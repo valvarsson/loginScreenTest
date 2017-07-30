f58dfb896fff247ba56b72dbe5ed4d41669e7ef0
valvarsson <viktor.alvarsson@icloud.com>
2017-07-30 17:33:27 +0200

    second awesome comment

--- loginscreen/ViewController.swift
+++ loginscreen/ViewController.swift
@@ -20,6 +20,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 // I also added this awesome comment!
+    
+// How many comments can you make?!
 
 }
 
